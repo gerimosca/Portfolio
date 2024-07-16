f782cffa02567b45bec9e2e43300f95ffebebad5
gerardo.mosca <gerimosca90@gmail.com>
2024-07-16 11:04:52 +0200

    primer commit

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const toggleThemeElement = document.getElementById('toggle-theme');
+const toggleIconElement = document.getElementById('toggle-icon');
+const toggleTextElement = document.getElementById('toggle-text');
+
+const toggleColorsElement = document.getElementById('toggle-colors');
+
+toggleThemeElement.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  if (toggleIconElement.src.includes('moon.svg')) {
+    toggleIconElement.src = './docs/assets/images/sun.svg';
+    toggleTextElement.textContent = 'Light Mode';
+  } else {
+    toggleIconElement.src = './docs/assets/images/moon.svg';
+    toggleTextElement.textContent = 'Dark Mode';
+  }
+});
+
+toggleColorsElement.addEventListener('click', (event) => {
+  console.dir(event);
+});
